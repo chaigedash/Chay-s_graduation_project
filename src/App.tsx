798b76114b03a9e83b81d915c03354477c37e9a0
pLittleChay <2338523922@qq.com>
2025-04-08 10:23:35 +0800

    初始化项目：低空经济移动端应用

---
+++ src/App.tsx
@@ -0,0 +1,68 @@
+import { TabBar } from 'antd-mobile'
+import {
+  HomeOutlined,
+  AppstoreOutlined,
+  FileTextOutlined,
+  BarChartOutlined,
+  UserOutlined
+} from '@ant-design/icons'
+import { useState } from 'react'
+import './App.css'
+import Resource from './components/Resource/Resource'
+
+function App() {
+  const [activeKey, setActiveKey] = useState('home')
+
+  const tabs = [
+    {
+      key: 'home',
+      title: '主页',
+      icon: <HomeOutlined />
+    },
+    {
+      key: 'resource',
+      title: '资源',
+      icon: <AppstoreOutlined />
+    },
+    {
+      key: 'exam',
+      title: '试题',
+      icon: <FileTextOutlined />
+    },
+    {
+      key: 'data',
+      title: '数据',
+      icon: <BarChartOutlined />
+    },
+    {
+      key: 'user',
+      title: '用户',
+      icon: <UserOutlined />
+    }
+  ]
+
+  return (
+    <div className="app-container">
+      <div className="content">
+        {activeKey === 'home' && (
+          <div className="home-container">
+            <div className="logo">
+              <span>低</span>
+              <span>空</span>
+              <span>经</span>
+              <span>济</span>
+            </div>
+          </div>
+        )}
+        {activeKey === 'resource' && <Resource />}
+      </div>
+      <TabBar activeKey={activeKey} onChange={setActiveKey}>
+        {tabs.map(item => (
+          <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+        ))}
+      </TabBar>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Resource/Resource.tsx
@@ -0,0 +1,73 @@
+import { Button, List } from 'antd-mobile'
+import { UploadOutlined, SettingOutlined } from '@ant-design/icons'
+import './Resource.css'
+
+const Resource = () => {
+  const resources = [
+    {
+      id: 1,
+      title: '低空经济发展白皮书2023',
+      date: '2024-01-20',
+      type: '文档'
+    },
+    {
+      id: 2,
+      title: '低空旅游产业解析',
+      date: '2024-01-18',
+      type: '视频'
+    },
+    {
+      id: 3,
+      title: '通航产业发展报告',
+      date: '2024-01-15',
+      type: '文档'
+    },
+    {
+      id: 4,
+      title: '低空旅游航线规划指南',
+      date: '2024-01-12',
+      type: '文档'
+    },
+    {
+      id: 5,
+      title: '低空经济示范区建设实录',
+      date: '2024-01-10',
+      type: '视频'
+    }
+  ]
+
+  return (
+    <div className="resource-container">
+      <div className="resource-header">
+        <Button className="header-button">
+          <UploadOutlined /> 上传资源
+        </Button>
+        <Button className="header-button">
+          <SettingOutlined /> 管理资源
+        </Button>
+      </div>
+
+      <List className="resource-list">
+        {resources.map(item => (
+          <List.Item
+            key={item.id}
+            description={item.date}
+            extra={item.type}
+          >
+            {item.title}
+          </List.Item>
+        ))}
+      </List>
+
+      <div className="pagination" style={{
+        backgroundColor: '#333',
+      }}>
+        <Button>上一页</Button>
+        <span className="page-info">第 1 页 / 共 1 页</span>
+        <Button>下一页</Button>
+      </div>
+    </div>
+  )
+}
+
+export default Resource
